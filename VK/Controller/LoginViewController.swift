92280af76f14ef377dcc67a37096c030177dad2e
Valeriia Sandetska <lerizlot@icloud.com>
2022-05-03 21:26:57 +0400

    minor fixes

---
+++ VK/Controller/LoginViewController.swift
@@ -0,0 +1,35 @@
+//
+//  ViewController.swift
+//  VK
+//
+//  Created by Lera on 27.04.22.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        print(#function)
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        print(#function)
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        print(#function)
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        print(#function)
+    }
+    
+    override func viewDidDisappear(_ animated: Bool) {
+        print(#function)
+    }
+    
+}
+
