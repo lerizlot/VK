0c7865c31dd5ae0dd1bb9af916b4290a4b8f52b1
Valeriia Sandetska <lerizlot@icloud.com>
2022-05-04 08:09:51 +0400

    added IBOutlets

--- VK/Controller/LoginViewController.swift
+++ VK/Controller/LoginViewController.swift
@@ -9,27 +9,26 @@ import UIKit
 
 class LoginViewController: UIViewController {
     
+    @IBOutlet weak var scrollView: UIScrollView!
+    @IBOutlet weak var logoImage: UIImageView!
+    
+    @IBOutlet weak var loginLabel: UILabel!
+    @IBOutlet weak var passwordLabel: UILabel!
+    
+    @IBOutlet weak var loginTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    
+    @IBAction func loginButton(_ sender: Any) {
+    }
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        print(#function)
+
     }
-    
-    override func viewWillAppear(_ animated: Bool) {
-        print(#function)
-    }
-    
-    override func viewDidAppear(_ animated: Bool) {
-        print(#function)
-    }
-    
-    override func viewWillDisappear(_ animated: Bool) {
-        print(#function)
-    }
-    
-    override func viewDidDisappear(_ animated: Bool) {
-        print(#function)
-    }
+  
     
 }
 
