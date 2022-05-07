a0d2164bc83d05a1ed459426e038f8a9bf7f9bad
Valeriia Sandetska <lerizlot@icloud.com>
2022-05-07 12:45:36 +0400

    IBOutlets fixed

--- VK/LoginViewController.swift
+++ VK/LoginViewController.swift
@@ -12,9 +12,20 @@ class LoginViewController: UIViewController {
     @IBOutlet weak var scrollView: UIScrollView!
     @IBOutlet weak var logoImage: UIImageView!
 
+    @IBOutlet weak var signInLabel: UILabel!
     
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
     
+    @IBAction func logInButton(_ sender: Any) {
+    }
     
+    @IBOutlet weak var orLabel: NSLayoutConstraint!
+    
+    @IBAction func appleSignInButton(_ sender: Any) {
+    }
+    @IBAction func signUpButton(_ sender: Any) {
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
